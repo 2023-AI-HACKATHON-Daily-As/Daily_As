977e754d684aab05649f980afffecf459863473e
yuni <shizume.dev@gmail.com>
2023-07-24 21:19:07 +0900

    귀차니즘으로 Antd 컴포넌트 설치

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,13 @@
 'use client'
 
 import Image from 'next/image';
-import Link from 'next/link';
 
 import { useEffect, useState, useRef } from 'react';
 
 export default function Home() {
   return (
     <main className='main index-page'>
-      <Link href='/test'>테스트</Link>
+      <h1>Daily As</h1>
     </main>
   )
 }
