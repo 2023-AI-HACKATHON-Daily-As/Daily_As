7670c49e937f29780c02fe38a61f4d2d564f88c6
yuni <shizume.dev@gmail.com>
2023-07-27 23:24:15 +0900

    Chat 컴포넌트 생성

--- app/components/ChatBot.tsx
+++ app/components/ChatBot.tsx
@@ -7,48 +7,60 @@
 
 import Link from 'next/link';
 
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 
 import getContent from '@/app/content/brand.json';
+
+import TextMap from '@/app/register/TextMap_Register.json';
 
 import MessageBoxChatbot from '@/app/components/chatbotComponents/chatbotMessage'
 
 const Chat = () => {
-    // useEffect(() => {
-    //     const Body = document.documentElement.style;
+    
+    const chatingHead = useRef<HTMLDivElement>(null);
+    const chatingContent = useRef<HTMLDivElement>(null);
 
-    //     Body.setProperty('background', '#f5f5f5')
-    // }, [])
+    useEffect(() => {
+        setTimeout(() => {
+            chatingHead.current?.classList.add('animation');
+        }, 1000)
+
+        setTimeout(() => {
+            chatingHead.current?.classList.add('up__start')
+        }, 2000)
+
+        setTimeout(() => {
+            chatingContent.current?.classList.add('animation')
+        }, 3900)
+    }, []);
+
+    
     return (
         <div className='customer-service'>
             <div className='style--chating not-gpt'>
-                <div className='chating-header'>
+                <div className='chating-header' ref={chatingHead}> 
                     <div className='g-container'>
                         <div className='chating-icon'>
                             <img src={getContent.chatboticon}/>
                         </div>
-                        <div className='chating-info'>
-                            <h1 className='name'>{getContent.chatbot}</h1>
-                            <p className='desc'>만나서 반가워요! 저는 여러분의 도우미 {getContent.chatbot}에요! 앞으로 자주 만나요~</p>
-                        </div>
                     </div>
                 </div>
-                <div className='chating-content g-container'>
+                <div className='chating-content g-container' ref={chatingContent}>
                     <div className='chatBot-message'>
                         <div className='chatBot--profile'>
                             <img src={getContent.chatboticon}/>
                         </div>
                         <div className='chatBot--message'>
-                            <MessageBoxChatbot>안녕하세요!</MessageBoxChatbot>
-                            <MessageBoxChatbot>귀요미 윤</MessageBoxChatbot>
+                            <MessageBoxChatbot>{TextMap.Register_Message1}</MessageBoxChatbot>
+                            <MessageBoxChatbot>{TextMap.Register_Message2}</MessageBoxChatbot>
+                            <MessageBoxChatbot>{TextMap.Register_Message3}</MessageBoxChatbot>
                         </div>
                     </div>
-                    <div className='chatUser-message'>
+                    {/* <div className='chatUser-message'>
                         <div className='chatBot--message'>
                             <MessageBoxChatbot>넹</MessageBoxChatbot>
-                            <MessageBoxChatbot>녜</MessageBoxChatbot>
                         </div>
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </div>
