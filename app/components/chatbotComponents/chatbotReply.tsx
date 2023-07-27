7c0ecd3ab9e9bf0637e45f14a7b1c46e58eb4344
yuni <shizume.dev@gmail.com>
2023-07-27 23:24:25 +0900

    Chat 답장 컴포넌트

---
+++ app/components/chatbotComponents/chatbotReply.tsx
@@ -0,0 +1,13 @@
+/**
+ * ChatBot Component에 사용되는 컴포넌트입니다.
+ * 쓰여지는 컴포넌트로 이동하려면 아래의 경로로 진입하십시오.
+ * @/app/components/ChatBot.js
+ * 
+ * ChatBot Message Box (chatbot)
+ */
+
+const Reply = () => {
+    return (
+        <div className="reply-component"></div>
+    )
+}
