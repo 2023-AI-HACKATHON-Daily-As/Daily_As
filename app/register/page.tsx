040c3f1ff1e02a36c71cc604d2882599b1c455eb
yuni <shizume.dev@gmail.com>
2023-07-27 23:25:13 +0900

    회원가입 스타일 정렬

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,11 +1,20 @@
+'use client'
+
 import RegisterComponent from '@/app/components/Register_Component';
 import ChatbotCompoent from '@/app/components/ChatBot';
+import { useEffect } from 'react';
 
 const Register = () => {
+    useEffect(() => {
+        const head = document.querySelector('html')
+        
+        head?.classList.add('register')
+    }, [])
+
     return (
         <main className="main register-page">
             <ChatbotCompoent />
-            {/* <RegisterComponent /> */}
+            <RegisterComponent />
         </main>
     )
 }
