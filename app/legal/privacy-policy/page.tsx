7a68f9d4eed16d50eb609bc188c57b3908e542a3
yuni <shizume.dev@gmail.com>
2023-07-29 01:04:44 +0900

    개인정보 처리방침 페이지 생성

---
+++ app/legal/privacy-policy/page.tsx
@@ -0,0 +1,22 @@
+const Agreement = () => {
+    return (
+        <main className="agreement">
+            <div className="article-template g-container">
+                <div className="article-head">
+                    <h1>개인정보 처리방침</h1>
+                    <span>최종 업데이트: 2023년 8월 5일</span>
+                </div>
+                <div className="article-content">
+                    <p>Daily As(이하 '당사')의 서비스를 사용해 주셔서 감사합니다.</p>
+                    <p>당사는 회원님의 개인정보 보호에 관한 권리를 존중합니다. 본 개인정보 처리방침(이하 '약관')은 회원님의 개인정보를 수집, 처리, 이전, 보호하는 방법을 전반적으로 설명하는 약관을 제공하고 있습니다.
+                    <br/><br/>해당 약관은 관련 법률 및 규정, 지침, 그리고 당사의 내부 운영 방침에 따라 수정될 수 있습니다. 실질적으로 약관이 수정될 경우 별도의 방법을 통해서 회원님에게 고지합니다. 개정된 약관에 동의하지 않으신다면 당사가 제공하는 서비스에 대한 액세스 및 이용을 중지하여야 합니다. </p>
+
+                    <p>1. 개인정보 수집 방법</p>
+                    <p>당사는 회원님의 요청에 따라 서비스를 수집하거나 </p>
+                </div>
+            </div>
+        </main>
+    )
+}
+
+export default Agreement
