44266256c888489fd0657a0adf28ce3d23929366
yuni <shizume.dev@gmail.com>
2023-07-30 20:26:47 +0900

    Fast Refresh had to perform a full reload Error 고침

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,23 +6,23 @@ import '@/app/css/chatbot.css';
 import '@/app/css/sidebar.css';
 import '@/app/css/to-do.css';
 import '@/app/css/article.css';
-import '@/app/css/settings.css'
+import '@/app/css/settings.css';
+import '@/app/css/challenge.css'
 
 import type { Metadata } from 'next';
 
 import Settings from '@/app/components/Settings';
 
 export const metadata: Metadata = {
-  title: 'Daily As',
+  title: {
+    default: 'Daily As',
+    template: '%s | Daily As'
+  },
   description: 'None',
   viewport: 'width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no'
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="ko-KR">
       <body>
@@ -34,5 +34,7 @@ export default function RootLayout({
         </div>
       </body>
     </html>
-  )
+  );
 }
+
+export default RootLayout;
