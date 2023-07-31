df909129cded45a686f7f8451abe3b1fab093d8a
yuni <shizume.dev@gmail.com>
2023-08-01 02:13:57 +0900

    회원가입 UI 수정

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -4,18 +4,29 @@ import RegisterComponent from '@/app/components/Register_Component';
 import ChatbotCompoent from '@/app/components/ChatBot';
 import { useEffect } from 'react';
 
-const Register = () => {
-    useEffect(() => {
-        const head = document.querySelector('html')
-        
-        head?.classList.add('register')
-    }, [])
+import Image from 'next/image';
+import Login from '../components/Login_Component';
 
+const Register = () => {
     return (
-        <main className="main register-page">
-            <ChatbotCompoent />
-            <RegisterComponent />
-        </main>
+        <>
+            <header className='daily-header'>
+                <div className='brand-logo'>
+                    <img className="inner" src='/UI/img/logo.png'/>
+                </div>
+                <div className='daily-btn get-started'>로그인</div>
+            </header>
+            <main className="main register-page">
+                <div className='first-Section'>
+                    <div className='g-container'>
+                        <div className='register-head'>
+                            <h1>반가워요!<br/>Daily As에 오신 것을 환영해요.</h1>
+                        </div>
+                    </div>
+                </div>
+            </main>
+            {/* <Login /> */}
+        </>
     )
 }
 
