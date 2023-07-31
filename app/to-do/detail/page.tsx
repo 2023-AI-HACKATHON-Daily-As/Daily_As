22fbc6c5993636eec9c07f2095545deace6263fd
yuni <shizume.dev@gmail.com>
2023-08-01 02:14:07 +0900

    To-Do Detail 페이지 생성

---
+++ app/to-do/detail/page.tsx
@@ -0,0 +1,11 @@
+import SideBar from "@/app/components/Sidebar"
+
+const TodoDetail = () => {
+    return (
+        <main className="main">
+            <SideBar />
+        </main>
+    )
+}
+
+export default TodoDetail
