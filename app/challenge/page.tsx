a1c14cd91fd59076ae6285e4353142e02dedfaa7
yuni <shizume.dev@gmail.com>
2023-08-03 03:46:41 +0900

    동적으로 Modal이 오픈될 수 있도록 설정

--- app/challenge/page.tsx
+++ app/challenge/page.tsx
@@ -4,7 +4,23 @@ import SideBar from "@/app/components/Sidebar";
 
 import { Swiper, SwiperSlide } from "swiper/react";
 
-const Challenge = () => {
+import React, { useState } from 'react';
+
+import CreateChallenge from "@/app/components/CreateChallenge";
+
+const Challenge: React.FC = () => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const handleOpenModal = () => {
+        setIsModalOpen(true);
+    };
+
+    const handleCloseModal = () => {
+        setIsModalOpen(false);
+    };
+
+    const [count, setCount] = useState<number>(1);
+
     return (
         <main className="main">
             <SideBar />
@@ -43,17 +59,6 @@ const Challenge = () => {
                             <h2>공개된 챌린지</h2>
                         </div>
                         <div className="container">
-                            {/* <div className="header">
-                                <div className="header-search">
-                                    <input placeholder="챌린지 검색" className="search-input"/>
-                                    <div className="search-icon">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="26" height="25" viewBox="0 0 26 25" fill="none">
-                                            <path d="M21.2992 11.1842C21.2992 16.8117 16.7521 21.3684 11.1496 21.3684C5.54705 21.3684 1 16.8117 1 11.1842C1 5.55669 5.54705 1 11.1496 1C16.7521 1 21.2992 5.55669 21.2992 11.1842Z" stroke="#BDBDBD" stroke-width="2"/>
-                                            <line y1="-1" x2="9.28964" y2="-1" transform="matrix(0.706011 0.708201 -0.706011 0.708201 17.0525 18.4209)" stroke="#BDBDBD" stroke-width="2"/>
-                                        </svg>
-                                    </div>
-                                </div>
-                            </div> */}
                             <div className="body">
                                 <div className="challenge-item">
                                     <div className="cover-wrapper">
@@ -75,7 +80,10 @@ const Challenge = () => {
                             
                         </div>
                     </div>
+                    <button onClick={handleOpenModal}>Open Modal</button>
                 </div>
+                
+                <CreateChallenge isOpen={isModalOpen} onClose={handleCloseModal} />
             </div>
         </main>
     )
